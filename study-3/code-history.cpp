c43fd5bf1ba35bcf549b4eeec2bdc26fd8fdf143
Arrian Purcell <arrian.purcell@gmail.com>
2014-07-11 18:20:02 +1000

    Adding code cache.

---
+++ study-3/code-history.cpp
@@ -0,0 +1,66 @@
+#include "code-history.h"
+
+void CodeHistory::addCode(std::string code, long time)
+{
+	codeCache.push_back(std::pair<long,std::string>(time, code));
+}
+
+void CodeHistory::addSelection(std::string selection, long time)
+{
+	selectionCache.push_back(std::pair<long,std::string>(time, selection));	
+}
+
+void CodeHistory::addEvaluation(std::string evaluation, long time)
+{
+	evaluationCache.push_back(std::pair<long,std::string>(time, evaluation));
+}
+
+void CodeHistory::addError(std::string error, long time)
+{
+	errorCache.push_back(std::pair<long,std::string>(time, error));
+}
+
+std::string CodeHistory::getCodeAtTime(long time)
+{
+	std::string* previous = nullptr;
+	for(auto item : codeCache)
+	{
+		if(previous == nullptr)
+		{
+			previous = &(item.second);
+			continue;
+		}
+
+		if(item.first > time) break;
+		
+		previous = &(item.second);
+	}
+
+	if(previous) return *(previous);
+	return "";
+}
+
+std::string CodeHistory::getCodeCurrent()
+{
+	if(codeCache.empty()) return "";
+	return codeCache.back().second;
+}
+
+std::string CodeHistory::getCodePrevious()
+{
+	if(codeCache.size() < 2) return "";
+	return codeCache.end()[-2].second;
+}
+
+std::string CodeHistory::toString()
+{
+	std::stringstream buffer;
+	buffer << "Code Cache: " << codeCache.size() << std::endl;
+	buffer << "Selection Cache: " << selectionCache.size() << std::endl;
+	buffer << "Evaluation Cache: " << evaluationCache.size() << std::endl;
+	buffer << "Error Cache: " << errorCache.size() << std::endl;
+	return buffer.str();
+}
+
+
+
--- study-3/code-history.h
+++ study-3/code-history.h
@@ -1,5 +1,11 @@
 #pragma once
 
+#include <time.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+
 
 class CodeHistory
 {
@@ -7,12 +13,41 @@ public:
 	long startTime;
 	long lastTime;
 
-	std::vector<std::string> cache;
+	std::vector<std::pair<long, std::string> > codeCache;
+	std::vector<std::pair<long, std::string> > selectionCache;
+	std::vector<std::pair<long, std::string> > evaluationCache;
+	std::vector<std::pair<long, std::string> > errorCache;
 
-	void add(std::string text);
-	
-	std::string getAtTime(long time);
-	std::string getCurrent();
-	std::string getPrevious();
+	void addCode(std::string code, long time = std::time(0));
+	void addSelection(std::string selection, long time = std::time(0));
+	void addEvaluation(std::string evaluation, long time = std::time(0));
+	void addError(std::string error, long time = std::time(0));
 
+	std::string getCodeAtTime(long time);
+	std::string getCodeCurrent();
+	std::string getCodePrevious();
+
+	std::string toString();
 };
+
+int main(int argc, char* argv[])
+{
+	CodeHistory codeHistory;
+
+	codeHistory.addCode("(test text)");
+	codeHistory.addSelection("1,1,1,4:1,4");//selections:cursor positions
+	codeHistory.addEvaluation("test");
+	codeHistory.addError("failed to evaluate test");
+	codeHistory.addSelection("1,1,1,4");
+	codeHistory.addEvaluation("(test text)");
+
+	std::cout << "Current Time: " << std::time(0) << std::endl;
+	std::cout << "Current Code: " << codeHistory.getCodeCurrent() << std::endl;
+	std::cout << "Previous Code: " << codeHistory.getCodePrevious() << std::endl;
+	std::cout << "At Time 0 Code: " << codeHistory.getCodeAtTime(0) << std::endl;
+
+	return 0;
+}
+
+
+
