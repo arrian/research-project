9b075b5a0fd82521a85a131fc65871e0731af572
Arrian Purcell <arrian.purcell@gmail.com>
2014-08-19 21:04:24 +1000

    Removing old app stuff.

--- study-3/app2-lib/PolycodeApp.h
+++ study-3/app2-lib/PolycodeApp.h
@@ -55,18 +55,6 @@ extern "C"
 	poly_scene_primitive* poly_scene_primitive_create_circle(double xSize, double ySize, int segments);
 	void poly_scene_primitive_destroy(poly_scene_primitive* primitive);
 	void poly_scene_primitive_set_circle_options(poly_scene_primitive* primitive, double xSize, double ySize, int segments);
-	
-	//Attractors - requires a physics enabled scene
-	void poly_attractors_update();
-
-	struct poly_attractor;
-	poly_attractor* poly_attractor_add(poly_scene* scene, poly_attractor* attractor, double x, double y, double size, bool isStatic);
-	void poly_attractor_remove(poly_attractor* attractor);
-	// void poly_attractor_pulse_size(double next, double final);
-	// void poly_attractor_pulse_color(double nr, double ng, double nb, double na, double fr, double fg, double fb, double fa);
-	void poly_attractor_target_size(poly_attractor* a, double size);
-	void poly_attractor_target_color(poly_attractor* att, double r, double g, double b, double a);
-	void poly_attractor_impulse(poly_attractor* a, double x, double y);
 }
 
 int main(int argc, char* argv[])
@@ -74,52 +62,21 @@ int main(int argc, char* argv[])
 	int width = 640;
 	int height = 480;
 
-	poly_core* core = poly_core_create("Test Window", width, height, "../resources/default.pak");
+	char windowName[] = "Test Window";
+	char resourcePath[] = "../resources/default.pak";
+
+	poly_core* core = poly_core_create(windowName, width, height, resourcePath);
 	poly_scene* scene = poly_scene_create(width, height);
 
-// poly_attractor_add(scene, nullptr, 100.0, 100.0, 100.0);
-
-	// poly_scene_multi_label* label = poly_scene_multi_label_create("test text\nnext line here\nanother line\n\na line after an empty line", 20);
-
-	//poly_scene_add_child(scene, (poly_entity*) poly_scene_label_create("test text", 30));
-	//poly_scene_add_child(scene, (poly_entity*) poly_scene_image_create("../resources/gradient.png"));
-	//poly_scene_add_child(scene, (poly_entity*) label);
-
-	//poly_scene_add_child(scene, (poly_entity*) poly_scene_primitive_create_cube(30.0, 30.0, 20.0));
-
 	int i = 0;
-	std::vector<poly_attractor*> attractors;
 	int iterations = 0;
 	int maxSize = 20.0;
 	int maxEntities = 100;
 	bool updateAttractors = true;
 
-	poly_attractor* a = poly_attractor_add(scene, nullptr, 0.0, 0.0,  RANDOM_NUMBER * maxSize, true);	
-	
-	poly_attractor_target_color(a, 0.0, 1.0, 0.0, 1.0);
-
 	while(true)
 	{
-		// std::cout << iterations << std::endl;
 		poly_core_update(core);
-		if(updateAttractors) poly_attractors_update();
-
-		if(i < maxEntities && iterations % 1 == 0) //20
-		{
-			attractors.push_back(poly_attractor_add(scene, nullptr, RANDOM_NUMBER * width - width / 2.0, RANDOM_NUMBER * height - height / 2.0,  RANDOM_NUMBER * maxSize + 20.0, false));	
-			i++;
-		}
-
-		if(attractors.size() > 0 && iterations % 2 == 0 && iterations > 1000)
-		{
-			// poly_attractor_target_size(attractors[int(RANDOM_NUMBER * double(attractors.size() - 1))], 200.0);
-			poly_attractor_target_color(attractors[int(RANDOM_NUMBER * double(attractors.size() - 1))], RANDOM_NUMBER, RANDOM_NUMBER / 2.0, RANDOM_NUMBER / 2.0, 1.0);
-		}
-
-		if(iterations == 500)
-		{
-			poly_attractor_target_size(a, 200.0);
-		}
 		
 		usleep(100);
 		iterations++;
